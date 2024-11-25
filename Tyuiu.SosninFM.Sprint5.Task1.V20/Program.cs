42fe449841c1051d14ef1bed5e73638827806bb7
SosninFedor <sosninfedor@gmail.com>
2024-11-25 18:09:41 +0500

    Task1

--- Tyuiu.SosninFM.Sprint5.Task1.V20/Program.cs
+++ Tyuiu.SosninFM.Sprint5.Task1.V20/Program.cs
@@ -7,7 +7,15 @@ namespace Tyuiu.SosninFM.Sprint5.Task1.V20
 	{
 		static void Main(string[] args)
 		{
-			
+			int startValue = -5;
+			int endValue = 5;
+
+			DataService ds = new DataService();
+
+			string res = ds.SaveToFileTextData(startValue, endValue);
+			Console.WriteLine("Файл: " + res);
+			Console.WriteLine("Создан!");
+			Console.ReadKey();
 
 
 
