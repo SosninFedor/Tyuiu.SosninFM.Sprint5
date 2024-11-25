8273027909ad9a712f3e38ba5c908dda0d97f092
SosninFedor <sosninfedor@gmail.com>
2024-11-25 21:56:52 +0500

    Task2

--- Tyuiu.SosninFM.Sprint5.Task2.V27/Program.cs
+++ Tyuiu.SosninFM.Sprint5.Task2.V27/Program.cs
@@ -7,7 +7,37 @@ namespace Tyuiu.SosninFM.Sprint5.Task2.V27
 	{
 		static void Main(string[] args)
 		{
-			
+			int[,] matrix = new int[3, 3]
+			{{1, 4, 3 },
+			 {1, 1, 4 },
+			 {4, 3, 8 },
+						};
+			int rows = matrix.GetUpperBound(0) + 1;
+			int columns = matrix.Length / rows;
+
+			DataService ds = new DataService();
+
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("Массив:");
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < columns; j++)
+				{
+					Console.Write($"{matrix[i, j]} \t");
+				}
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+			Console.WriteLine("***************************************************************************");
+			string res = ds.SaveToFileTextData(matrix);
+			Console.WriteLine("Файл " + res);
+			Console.WriteLine("Создан!");
+			Console.ReadKey();
+
 		}
 	}
 }
