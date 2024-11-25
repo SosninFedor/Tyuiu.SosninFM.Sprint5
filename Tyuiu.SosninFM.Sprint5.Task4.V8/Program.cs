290472ed81814c9c383b1c5e636d84ec9e891560
SosninFedor <sosninfedor@gmail.com>
2024-11-26 02:31:17 +0500

    Task4

--- Tyuiu.SosninFM.Sprint5.Task4.V8/Program.cs
+++ Tyuiu.SosninFM.Sprint5.Task4.V8/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Tyuiu.SosninFM.Sprint5.Task4.V8
+﻿using Tyuiu.SosninFM.Sprint5.Task4.V8.Lib;
+
+namespace Tyuiu.SosninFM.Sprint5.Task4.V8
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			
+			DataService ds = new DataService();
+
+			Console.WriteLine("****************************************************************************************************");
+			Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                 *");
+			Console.WriteLine("****************************************************************************************************");
+
+			string path = @"С:\DataSprint5\InPutDataFileTask4V8.txt";
+			Console.WriteLine("Данные находятся в файле: " + path);
+
+			Console.WriteLine("****************************************************************************************************");
+			Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                       *");
+			Console.WriteLine("****************************************************************************************************");
+
+			double res = ds.LoadFromDataFile(path);
+			Console.WriteLine(res);
+			Console.ReadKey();
 		}
 	}
 }
