80bec9df0cb0704ad9bc0ecba3567f3dedea30d3
SosninFedor <sosninfedor@gmail.com>
2024-11-25 22:40:15 +0500

    Task3

--- Tyuiu.SosninFM.Sprint5.Task3.V7.Lib/DataService.cs
+++ Tyuiu.SosninFM.Sprint5.Task3.V7.Lib/DataService.cs
@@ -9,14 +9,15 @@ namespace Tyuiu.SosninFM.Sprint5.Task3.V7.Lib
 	{
 		public string SaveToFileTextData(int x)
 		{
-			string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
-			double y = (1.6 * (Math.Pow(x, 3)) - 2.1 * (Math.Pow(x, 2)) + 7 * x);
-			y = Math.Round(y, 3);
-			using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
-			{
-				writer.Write(BitConverter.GetBytes(y));
-			}
-			return path;
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Append)))
+            {
+                writer.Write(1.6 * (Math.Pow(x, 3)) - 2.1 * (Math.Pow(x, 2)) + 7 * x);
+
+            }
+
+            return path;
 
 		}
 	}
