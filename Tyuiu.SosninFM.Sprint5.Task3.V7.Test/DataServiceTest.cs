21901019c96b89968363e16fc5473818f6e6a9bb
SosninFedor <sosninfedor@gmail.com>
2024-11-25 22:15:18 +0500

    Task3

---
+++ Tyuiu.SosninFM.Sprint5.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿
+using Tyuiu.SosninFM.Sprint5.Task3.V7.Lib;
+
+namespace Tyuiu.SosninFM.Sprint5.Task3.V7.Test
+{
+	[TestClass]
+	public sealed class DataServiceTest
+	{
+		[TestMethod]
+		public void CheckExistsFile()
+		{
+			string path = @"C:\Users\sosni\AppData\Local\Temp\OutPutFileTask2.bin";
+
+			FileInfo fileInfo = new FileInfo(path);
+			bool fileExists = fileInfo.Exists;
+			bool wait = false;
+
+			Assert.AreEqual(wait, fileExists);
+		}
+	}
+}
