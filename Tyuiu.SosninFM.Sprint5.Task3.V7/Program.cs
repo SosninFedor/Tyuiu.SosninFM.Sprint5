4129a6f11ca8b7d82157b9217257bd668377d505
SosninFedor <sosninfedor@gmail.com>
2024-11-25 22:28:40 +0500

    Task3

--- Tyuiu.SosninFM.Sprint5.Task3.V7/Program.cs
+++ Tyuiu.SosninFM.Sprint5.Task3.V7/Program.cs
@@ -7,7 +7,21 @@ namespace Tyuiu.SosninFM.Sprint5.Task3.V7
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			int x = 2;
+			DataService ds = new DataService();
+
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("x = " + x);
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+			Console.WriteLine("***************************************************************************");
+			string res = ds.SaveToFileTextData(x);
+			Console.WriteLine("Файл " + res);
+			Console.WriteLine("Создан!");
+			Console.ReadKey();
+
 		}
 	}
 }
