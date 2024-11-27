d27f5a016587a3407cc00dd265ee5d10b35c6545
SosninFedor <sosninfedor@gmail.com>
2024-11-27 13:11:47 +0500

    Task5

--- Tyuiu.SosninFM.Sprint5.Task5.V28.Lib/DataService.cs
+++ Tyuiu.SosninFM.Sprint5.Task5.V28.Lib/DataService.cs
@@ -8,34 +8,27 @@ namespace Tyuiu.SosninFM.Sprint5.Task5.V28.Lib
 	{
 		public double LoadFromDataFile(string path)
 		{
-			double ans = double.MinValue;
-
+			double result = 1;
+			double minNum = 0;
+			using (StreamReader reader = new StreamReader(path))
 			{
-				double result = 1;
-				double minNum = 0;
-
-				using (StreamReader reader = new StreamReader(path))
+				string line;
+				List<double> minArray = new List<double>();
+				while ((line = reader.ReadLine()) != null)
 				{
-					string line;
-					List<double> minArray = new List<double>();
-					while ((line = reader.ReadLine()) != null)
-					{
 
-						if (Convert.ToDouble(line) > 0 && Convert.ToDouble(line) % 5 == 0)
-						{
-							minArray.Add(Convert.ToDouble(line));
-						}
-					}
-					minNum = minArray.Min();
-					for (int i = 1; i <= minNum; i++)
+					if (Convert.ToDouble(line) > 0 && Convert.ToDouble(line) % 5 == 0)
 					{
-						result *= i;
+						minArray.Add(Convert.ToDouble(line));
 					}
 				}
-				return result;
-
+				minNum = minArray.Min();
+				for (int i = 1; i <= minNum; i++)
+				{
+					result *= i;
+				}
 			}
-
+			return result;
 		}
 	}
 }
