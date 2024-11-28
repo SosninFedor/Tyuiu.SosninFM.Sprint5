376110bbb0c05ca43a96b96586c7e2cd196ea99e
SosninFedor <sosninfedor@gmail.com>
2024-11-28 18:25:23 +0500

    Task5

--- Tyuiu.SosninFM.Sprint5.Task5.V28.Lib/DataService.cs
+++ Tyuiu.SosninFM.Sprint5.Task5.V28.Lib/DataService.cs
@@ -12,27 +12,53 @@ namespace Tyuiu.SosninFM.Sprint5.Task5.V28.Lib
 	{
 		public double LoadFromDataFile(string path)
 		{
-			double factorial = 1;
-			double s = double.MaxValue;
+			double ans = double.MinValue;
 
 			using (StreamReader reader = new StreamReader(path))
 			{
 				string line;
+
 				while ((line = reader.ReadLine()) != null)
 				{
-					double number = Convert.ToDouble(line);
-					if (number > 0 && number % 5 == 0 && number < s)
+					string[] arr = line.Split(' ');
+					foreach (string value2 in arr)
 					{
-						s = number;
-						for (int i = 1; i <= s; i++)
+						bool check = false;
+						foreach (char tempchar in value2)
 						{
-							factorial *= i;
+							if (tempchar == '.' || tempchar == ',')
+							{
+								check = true;
+							}
+						}
+						if (check)
+						{
+							continue;
+						}
+						int value = Convert.ToInt32(value2);
+						if (value == 2)
+						{
+							ans = Math.Max(ans, value);
+							continue;
+						}
+
+						if (value > ans)
+						{
+							bool temp = true;
+							for (int i = 2; i < Math.Pow(value, 0.5) + 1; i++)
+							{
+								if (value % i == 0)
+								{
+									temp = false;
+									break;
+								}
+							}
+							if (temp) ans = value;
 						}
 					}
 				}
 			}
-
-			return Math.Round(factorial, 3);
+			return Math.Round(ans, 3);
 		}
 	}
 }
