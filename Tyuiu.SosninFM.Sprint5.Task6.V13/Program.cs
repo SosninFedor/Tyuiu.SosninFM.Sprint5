caee80ff67231dd632da2b10c1c315b3f7ad3b0f
SosninFedor <sosninfedor@gmail.com>
2024-11-28 23:16:31 +0500

    Task6

--- Tyuiu.SosninFM.Sprint5.Task6.V13/Program.cs
+++ Tyuiu.SosninFM.Sprint5.Task6.V13/Program.cs
@@ -7,7 +7,19 @@ namespace Tyuiu.SosninFM.Sprint5.Task6.V13
 	{
 		static void Main(string[] args)
 		{
-			
+			DataService ds = new DataService();
+
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+			Console.WriteLine("***************************************************************************");
+			string path = @"C:\DataSprint5\InPutDataFileTask6V13.txt";
+			Console.WriteLine("Данные находятся в файле: " + path);
+
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+			Console.WriteLine("***************************************************************************");
+			Console.WriteLine("В строке находится " + ds.LoadFromDataFile(path) + " удвоенных букв \"с\"");
+			Console.ReadKey();
 
 
 
